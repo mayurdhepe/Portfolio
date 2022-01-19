765def2ea218f5f7aa0dc86f773df3f437beac40
Mayur Dhepe <mayurdhepe@gmail.com>
2022-01-18 21:27:40 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'https://github.com/mayurdhepe/Portfolio/blob/main/public/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
